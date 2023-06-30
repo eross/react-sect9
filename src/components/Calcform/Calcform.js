4077ed21cc7b5d849e42fa02676dceff5ff81d47
Eric Ross <epr@rossmbox.com>
2023-06-29 18:39:44 -0700

    Add change handler

--- src/components/Calcform/Calcform.js
+++ src/components/Calcform/Calcform.js
@@ -2,44 +2,54 @@ import React from 'react';
 
 const CalcForm = () => {
   const inputChangeHandler = (identifier, value) => {
+    console.log(identifier);
     console.log(value);
   };
   return (
     <form className="form">
-    <div className="input-group">
-      <p>
-        <label htmlFor="current-savings">Current Savings ($)</label>
-        <input type="number" id="current-savings"
-         onchange = {(event => {
-          inputChangeHandler('current-savings', event.target.value);
-         })}/>
+      <div className="input-group">
+        <p>
+          <label htmlFor="current-savings">Current Savings ($)</label>
+          <input type="number" id="current-savings"
+            onChange={(event => {
+              inputChangeHandler('current-savings', event.target.value);
+            })} />
+        </p>
+        <p>
+          <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
+          <input type="number" id="yearly-contribution"
+            onChange={(event => {
+              inputChangeHandler('yearly-contribution', event.target.value);
+            })} />
+        </p>
+      </div>
+      <div className="input-group">
+        <p>
+          <label htmlFor="expected-return">
+            Expected Interest (%, per year)
+          </label>
+          <input type="number" id="expected-return"
+            onChange={(event => {
+              inputChangeHandler('expected-return', event.target.value);
+            })} />
+        </p>
+        <p>
+          <label htmlFor="duration">Investment Duration (years)</label>
+          <input type="number" id="duration"
+            onChange={(event => {
+              inputChangeHandler('expected-return', event.target.value);
+            })} />
+        </p>
+      </div>
+      <p className="actions">
+        <button type="reset" className="buttonAlt">
+          Reset
+        </button>
+        <button type="submit" className="button">
+          Calculate
+        </button>
       </p>
-      <p>
-        <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
-        <input type="number" id="yearly-contribution" />
-      </p>
-    </div>
-    <div className="input-group">
-      <p>
-        <label htmlFor="expected-return">
-          Expected Interest (%, per year)
-        </label>
-        <input type="number" id="expected-return" />
-      </p>
-      <p>
-        <label htmlFor="duration">Investment Duration (years)</label>
-        <input type="number" id="duration" />
-      </p>
-    </div>
-    <p className="actions">
-      <button type="reset" className="buttonAlt">
-        Reset
-      </button>
-      <button type="submit" className="button">
-        Calculate
-      </button>
-    </p>
-  </form>
+    </form>
   )
 };
 
